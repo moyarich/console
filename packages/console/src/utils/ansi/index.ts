5237b954db49ea7a68948005ee91262d00c2cbc3
moyarich <moyarich@hotmail.com>
2026-09-22 08:47:35 -0500

    Add ANSI utility aggregation entry

---
+++ packages/console/src/utils/ansi/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Low-level ANSI parsing and styling helpers.
+ */
+export { ANSI_CLEAR_LINE_PATTERN, ANSI_ESCAPE } from "./constants";
+export { getAnsiTokenRanges } from "./getAnsiTokenRanges";
+export { getAnsiTokenStyle } from "./getAnsiTokenStyle";
+export { hasAnsiClearLine } from "./hasAnsiClearLine";
+export type { AnserToken, AnsiTokenRange } from "./types";
