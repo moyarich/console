b17e917621fe57fcd242cddf7efd8e3c69396620
moyarich <moyarich@hotmail.com>
2026-09-22 08:51:01 -0500

    Add renderer domain aggregation entry

---
+++ packages/console/src/renderers/index.ts
@@ -0,0 +1,10 @@
+/** Public renderer extension contracts and dispatch helpers. */
+export { dispatchMessageRenderer } from "./dispatchMessageRenderer";
+export { dispatchValueRenderer } from "./dispatchValueRenderer";
+export { getConsoleValueType } from "./getConsoleValueType";
+export type {
+  ConsoleMessageRenderer,
+  ConsoleMessageRendererContext,
+  ConsoleValueRenderer,
+  ConsoleValueRendererContext,
+} from "./types";
