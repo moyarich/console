17d4f1bb37bd16a4e1c4b58d8c0885d3fa555cda
moyarich <moyarich@hotmail.com>
2026-09-22 07:00:37 -0500

    Add index.ts utility entry point

---
+++ packages/console/src/utils/console/index.ts
@@ -0,0 +1,35 @@
+/**
+ * Structured-console utility modules.
+ *
+ * These helpers are available through the secondary package entry point
+ * `@moyarich/console/utils/console` without expanding the main package API.
+ */
+export {
+  CONTEXT_MENU_THEME_PROPERTIES,
+  CONTEXT_MENU_VIEWPORT_MARGIN,
+} from "./constants";
+export { allRowsAreRecords } from "./allRowsAreRecords";
+export { collectColumns } from "./collectColumns";
+export { formatConsoleObjectForCopy } from "./formatConsoleObjectForCopy";
+export { getContextMenuThemeStyle } from "./getContextMenuThemeStyle";
+export { getEventPoint } from "./getEventPoint";
+export { getMenuPosition } from "./getMenuPosition";
+export { isElementLike } from "./isElementLike";
+export { isInspectableObject } from "./isInspectableObject";
+export { isMapLike } from "./isMapLike";
+export { isObjectLike } from "./isObjectLike";
+export { isRecord } from "./isRecord";
+export { isSetLike } from "./isSetLike";
+export { isTableRecord } from "./isTableRecord";
+export { normalizeConsoleTableData } from "./normalizeConsoleTableData";
+export { normalizeConsoleValue } from "./normalizeConsoleValue";
+export { objectEntries } from "./objectEntries";
+export { objectLabel } from "./objectLabel";
+export { preview } from "./preview";
+export { toRows } from "./toRows";
+export { typeClass } from "./typeClass";
+export type {
+  ConsoleTableRow,
+  ContextMenuThemeProperty,
+  ContextMenuThemeStyle,
+} from "./types";
