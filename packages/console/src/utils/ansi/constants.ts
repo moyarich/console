d51a8ede689c9f60d80a21c28a54dccdeedf5d4d
moyarich <moyarich@hotmail.com>
2026-09-22 08:48:38 -0500

    Fix ANSI clear-line regex source

--- packages/console/src/utils/ansi/constants.ts
+++ packages/console/src/utils/ansi/constants.ts
@@ -1,7 +1,7 @@
 /** ASCII escape character used to begin ANSI control sequences. */
 export const ANSI_ESCAPE = String.fromCharCode(27);
 
-const ANSI_CLEAR_LINE_SOURCE = `${ANSI_ESCAPE}\\\\[[012]?K`;
+const ANSI_CLEAR_LINE_SOURCE = `${ANSI_ESCAPE}\\[[012]?K`;
 
 /** Matches ANSI erase-in-line controls used by redraw/progress output. */
 export const ANSI_CLEAR_LINE_PATTERN = new RegExp(ANSI_CLEAR_LINE_SOURCE);
