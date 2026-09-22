62fb75beb37fcf404c91528fd9fe20f985308d14
moyarich <moyarich@hotmail.com>
2026-09-22 08:46:13 -0500

    Move index.ts into table utilities

---
+++ packages/console/src/utils/table/index.ts
@@ -0,0 +1,5 @@
+/** Reusable console.table normalization and rendering helpers. */
+export { collectColumns } from "./collectColumns";
+export { normalizeConsoleTableData } from "./normalizeConsoleTableData";
+export { toRows } from "./toRows";
+export type { ConsoleTableRow } from "./types";
