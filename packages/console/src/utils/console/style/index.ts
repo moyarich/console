481ef3bcb0002b2d2bd5fcd9e0407ad0969f8271
moyarich <moyarich@hotmail.com>
2026-09-22 07:33:12 -0500

    Add console style index.ts module

---
+++ packages/console/src/utils/console/style/index.ts
@@ -0,0 +1,8 @@
+/** Reusable structured-console styling helpers and theme types. */
+export { CONTEXT_MENU_THEME_PROPERTIES } from "./constants";
+export { getContextMenuThemeStyle } from "./getContextMenuThemeStyle";
+export { typeClass } from "./typeClass";
+export type {
+  ContextMenuThemeProperty,
+  ContextMenuThemeStyle,
+} from "./types";
