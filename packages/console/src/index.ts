03695da9243efd389a99deab72fa5c758224a04a
moyarich <moyarich@hotmail.com>
2026-09-21 21:43:08 -0500

    feat: export process-output processor API

--- packages/console/src/index.ts
+++ packages/console/src/index.ts
@@ -35,6 +35,12 @@ export type {
   ConsoleStructuredOutputParser,
   ConsoleStructuredOutputParserContext,
 } from "./components/ConsoleStdout";
+export { processConsoleOutputEntry } from "./processOutput";
+export type {
+  ConsoleProcessOutput,
+  ConsoleProcessOutputProcessorContext,
+  ConsoleProcessOutputProcessorResult,
+} from "./processOutput";
 export { ConsoleValue } from "./components/ConsoleValue";
 export { ConsoleTable } from "./components/ConsoleTable";
 export { getConsoleValueType } from "./renderers";
