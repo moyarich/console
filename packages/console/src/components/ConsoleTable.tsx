9190edf0e353d45b2496039b79b2a3aac0c96e9d
moyarich <moyarich@hotmail.com>
2026-09-22 07:21:59 -0500

    Use console table utility directory

--- packages/console/src/components/ConsoleTable.tsx
+++ packages/console/src/components/ConsoleTable.tsx
@@ -3,8 +3,8 @@ import { useConsoleContextMenu } from "../hooks/useConsoleContextMenu";
 import { ConsoleValue } from "./ConsoleValue";
 import type { ConsoleValueRenderer } from "../renderers";
 import type { ConsoleLinkProvider } from "../links";
-import { collectColumns } from "../utils/console/collectColumns";
-import { toRows } from "../utils/console/toRows";
+import { collectColumns } from "../utils/console/table/collectColumns";
+import { toRows } from "../utils/console/table/toRows";
 import { isObjectLike } from "../utils/console/isObjectLike";
 
 /** Props for rendering normalized `console.table()` output. */
