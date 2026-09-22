175984fe21f497780c54106e4dfafde0a2e5a43a
moyarich <moyarich@hotmail.com>
2026-09-22 07:00:38 -0500

    Add index.ts utility entry point

---
+++ packages/console/src/utils/terminal/index.ts
@@ -0,0 +1,19 @@
+/**
+ * ANSI/process-output utility modules.
+ *
+ * These helpers are available through the secondary package entry point
+ * `@moyarich/console/utils/terminal` without expanding the main package API.
+ */
+export { ANSI_CLEAR_LINE_PATTERN, ANSI_ESCAPE } from "./constants";
+export { getAnsiTokenRanges } from "./getAnsiTokenRanges";
+export { getAnsiTokenStyle } from "./getAnsiTokenStyle";
+export { hasAnsiClearLine } from "./hasAnsiClearLine";
+export { parseStrictJsonOutput } from "./parseStrictJsonOutput";
+export { parseStructuredOutput } from "./parseStructuredOutput";
+export { stripAnsiText } from "./stripAnsiText";
+export type {
+  AnserToken,
+  AnsiTokenRange,
+  ConsoleStructuredOutputParser,
+  ConsoleStructuredOutputParserContext,
+} from "./types";
