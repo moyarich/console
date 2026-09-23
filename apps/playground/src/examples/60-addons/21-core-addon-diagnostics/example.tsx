b99315a3d49a6514e77f0191a3f41a712a0e7932
moyarich <moyarich@hotmail.com>
2026-09-23 08:14:35 -0500

    style: apply diagnostics example formatting

--- apps/playground/src/examples/60-addons/21-core-addon-diagnostics/example.tsx
+++ apps/playground/src/examples/60-addons/21-core-addon-diagnostics/example.tsx
@@ -24,11 +24,7 @@ const structuredMessages: ConsoleMessageData[] = [
   },
 ];
 
-const processMessages = [
-  "Progress 10%\r",
-  "Progress 60%\r",
-  "Progress 100%\n",
-];
+const processMessages = ["Progress 10%\r", "Progress 60%\r", "Progress 100%\n"];
 
 const processors: ConsoleProcessOutputProcessor[] = [
   {
