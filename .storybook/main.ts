ef9fa25e795b259ac585cf1b823edb60e749aef7
moyarich <moyarich@hotmail.com>
2026-09-22 22:57:02 -0500

    build(storybook): alias export addon source

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -27,6 +27,15 @@ const config: StorybookConfig = {
             ),
           },
           {
+            find: "@moyarich/console-addon-export",
+            replacement: fileURLToPath(
+              new URL(
+                "../packages/console-addon-export/src/index.ts",
+                import.meta.url,
+              ),
+            ),
+          },
+          {
             find: "@moyarich/console",
             replacement: fileURLToPath(
               new URL("../packages/console/src/index.ts", import.meta.url),
