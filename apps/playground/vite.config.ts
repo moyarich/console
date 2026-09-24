e877768390a421d839b5f9661aa7eaa25ce77f95
moyarich <moyarich@hotmail.com>
2026-09-24 16:49:34 -0500

    Use ESM workers for Monaco VS Code runtime

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -11,6 +11,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: process.env.VITE_BASE_PATH || "/",
+  worker: {
+    format: "es",
+  },
   plugins: [
     {
       enforce: "pre",
