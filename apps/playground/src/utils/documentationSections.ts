ea812fd510f85759c3136b3de835db97f1895233
moyarich <moyarich@hotmail.com>
2026-09-24 08:49:54 -0500

    refactor(playground): move documentation section registry

---
+++ apps/playground/src/utils/documentationSections.ts
@@ -0,0 +1,7 @@
+import { CONSOLE_API_SECTION } from "../api";
+import type { MdxSection } from "./mdxSection";
+
+export const DOCUMENTATION_SECTIONS: readonly MdxSection[] = [
+  CONSOLE_API_SECTION,
+];
+
