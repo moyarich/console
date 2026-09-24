82e841aa664dc00a10c804b0f51862a69937a9a8
moyarich <moyarich@hotmail.com>
2026-09-24 05:58:43 -0500

    feat(console): preserve scrollbar theme in portal

--- packages/console/src/utils/console/style/constants.ts
+++ packages/console/src/utils/console/style/constants.ts
@@ -4,7 +4,11 @@ import type { ContextMenuThemeProperty } from "./types";
 export const CONTEXT_MENU_THEME_PROPERTIES: readonly ContextMenuThemeProperty[] =
   [
     "--console-color-scheme",
+    "--console-scrollbar-width",
+    "--console-scrollbar-color",
     "--console-context-menu-color-scheme",
+    "--console-context-menu-scrollbar-width",
+    "--console-context-menu-scrollbar-color",
     "--console-context-menu-background-color",
     "--console-context-menu-border",
     "--console-context-menu-color",
