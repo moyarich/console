6d03942fd8558f49db7f86ff0d4dc059266f0ddc
moyarich <moyarich@hotmail.com>
2026-09-23 22:05:35 -0500

    refactor(storybook): rely on package-owned styles

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,4 @@
 import type { Preview } from "@storybook/react-vite";
-import "../packages/console/src/styles.css";
-import "../packages/addons/resizable/src/styles.css";
 
 const preview: Preview = {
   parameters: {
