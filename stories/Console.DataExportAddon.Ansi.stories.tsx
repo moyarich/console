e9711e8bb2a68eee6291c085861e1532a49291a3
moyarich <moyarich@hotmail.com>
2026-09-24 14:27:12 -0500

    refactor(process): use processor context

--- stories/Console.DataExportAddon.Ansi.stories.tsx
+++ stories/Console.DataExportAddon.Ansi.stories.tsx
@@ -14,7 +14,7 @@ const messages = [
 const processors: ConsoleProcessOutputProcessor[] = [
   {
     id: "storybook-build-status",
-    process(output) {
+    process({ output }) {
       if (!output.data.includes("Progress 100%")) return;
       return {
         data: output.data.replace("Progress 100%", "Build complete"),
