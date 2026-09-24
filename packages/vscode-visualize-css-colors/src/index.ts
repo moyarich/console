3f83330fa5d6bccaa7a82d30e9f215e0cfe0a39d
moyarich <moyarich@hotmail.com>
2026-09-24 15:40:08 -0500

    style(vscode-colors): format manifest exports

--- packages/vscode-visualize-css-colors/src/index.ts
+++ packages/vscode-visualize-css-colors/src/index.ts
@@ -2,7 +2,6 @@ import manifest from "../extension.manifest.json";
 
 export const vscodeVisualizeCssColorsManifest = manifest;
 
-export const vscodeVisualizeCssColorsExtensionId =
-  `${manifest.publisher}.${manifest.name}`;
+export const vscodeVisualizeCssColorsExtensionId = `${manifest.publisher}.${manifest.name}`;
 
 export const vscodeVisualizeCssColorsBrowserPath = manifest.browser;
