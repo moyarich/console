82ca6c9d700187af6647a7320a43787c465ade53
moyarich <moyarich@hotmail.com>
2026-09-24 15:40:06 -0500

    style(css-color-parser): format exports

--- packages/css-color-parser/src/index.ts
+++ packages/css-color-parser/src/index.ts
@@ -2,8 +2,4 @@ export { extractCssColors } from "./extractCssColors";
 export { CSS_NAMED_COLORS } from "./namedColors";
 export { parseCssColor } from "./parseCssColor";
 export type { ParsedCssColor } from "./parseCssColor";
-export type {
-  CssColorFormat,
-  CssColorMatch,
-  RgbaColor,
-} from "./types";
+export type { CssColorFormat, CssColorMatch, RgbaColor } from "./types";
