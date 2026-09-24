3d4df5bb7e32bf55c7944a6a6bb5f0a2b8f56c11
moyarich <moyarich@hotmail.com>
2026-09-23 21:18:32 -0500

    chore(storybook): alias resizable addon source

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -42,6 +42,15 @@ const config: StorybookConfig = {
             ),
           },
           {
+            find: "@moyarich/console-addon-resizable",
+            replacement: fileURLToPath(
+              new URL(
+                "../packages/addons/resizable/src/index.tsx",
+                import.meta.url,
+              ),
+            ),
+          },
+          {
             find: "@moyarich/console-addon-data-export",
             replacement: fileURLToPath(
               new URL(
