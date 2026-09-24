2a049d5cc4ce33e05d67f7df2dd4271d1b50bfe8
moyarich <moyarich@hotmail.com>
2026-09-24 08:50:30 -0500

    refactor(playground): import API helpers from utils

--- apps/playground/src/api/index.ts
+++ apps/playground/src/api/index.ts
@@ -1,4 +1,4 @@
-import { createMdxSection, type MdxPageModule } from "../content/mdxSection";
+import { createMdxSection, type MdxPageModule } from "../utils/mdxSection";
 
 const pageModules = import.meta.glob("./*/page.mdx", {
   eager: true,
