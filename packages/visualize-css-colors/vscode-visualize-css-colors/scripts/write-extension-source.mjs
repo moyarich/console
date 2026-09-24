726e4b515c6d5083a3bc7004b4c8e0cfdf61b49a
moyarich <moyarich@hotmail.com>
2026-09-24 16:42:57 -0500

    Generate importable web extension source

---
+++ packages/visualize-css-colors/vscode-visualize-css-colors/scripts/write-extension-source.mjs
@@ -0,0 +1,18 @@
+import { readFile, writeFile } from "node:fs/promises";
+
+const extensionUrl = new URL("../dist/extension.js", import.meta.url);
+const sourceModuleUrl = new URL("../dist/extension-source.js", import.meta.url);
+const sourceTypesUrl = new URL("../dist/extension-source.d.ts", import.meta.url);
+
+const extensionSource = await readFile(extensionUrl, "utf8");
+
+await Promise.all([
+  writeFile(
+    sourceModuleUrl,
+    `const extensionSource = ${JSON.stringify(extensionSource)};\n\nexport default extensionSource;\n`,
+  ),
+  writeFile(
+    sourceTypesUrl,
+    "declare const extensionSource: string;\n\nexport default extensionSource;\n",
+  ),
+]);
