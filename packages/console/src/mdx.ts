dbcb5e43e3e8e4b814d3aa29bd7155c87a9f95f5
moyarich <moyarich@hotmail.com>
2026-09-25 15:12:30 -0500

    feat: add console MDX component map

---
+++ packages/console/src/mdx.ts
@@ -0,0 +1,16 @@
+import {
+  ProcessOutput,
+  type ProcessOutputProps,
+} from "./components/ProcessOutput";
+
+/**
+ * MDX components owned by @moyarich/console.
+ *
+ * Documentation frameworks can merge this plain component map into their
+ * standard MDXProvider/useMDXComponents configuration.
+ */
+export const consoleMdxComponents = {
+  ProcessOutput,
+};
+
+export type { ProcessOutputProps };
