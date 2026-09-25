d48756e0123a5d66fb66346d49a62cbc34816325
moyarich <moyarich@hotmail.com>
2026-09-25 09:21:24 -0500

    export demo browser setup

--- packages/vscode-dev-toolkit/src/demo/index.mjs
+++ packages/vscode-dev-toolkit/src/demo/index.mjs
@@ -1,3 +1,4 @@
+export { installDemoBrowser } from "./browser.mjs";
 export { captureScenario } from "./codegen-recorder.mjs";
 export { createReadmeGifs, runGifCli } from "./gif.mjs";
 export { runInteractiveDemoCli } from "./interactive.mjs";
